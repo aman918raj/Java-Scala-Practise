dfabbbf5b5272dfd65780f6ec1f6eebbbad3b0bb
aman918raj <43367687+aman918raj@users.noreply.github.com>
2019-09-09 21:29:19 +0530

    9thSep2019 changes implicit added

---
+++ src/main/scala/ScalaImplicit.scala
@@ -0,0 +1,48 @@
+
+object ScalaImplicit {
+
+  /*
+  There are 3 types of use of Implicit
+  1. Implicitly type conversion : It converts the error producing assignment into intended type
+  val x :String = "1"
+  val y:Int = x
+  String is not the sub type of Int , so error happens in line 2.
+  To resolve the error the compiler will look for such a method in the scope
+  which has implicit keyword and takes a String as argument and returns an Int .
+
+  2. Implicitly parameter injection
+  If we call a method and do not pass its parameter value, it will cause an error.
+  If the parameter has any implicit keyword it will look for any val in the scope
+  which have the same type of value. If not get it will cause error.
+
+  3. Implicitly receiver conversion: To call any property by a receiver the property must be the
+     member of that receiver's class/object.
+   */
+  def main(args: Array[String]): Unit = {
+
+    //1.
+    implicit def doubleToInt(d: Double) = d.toInt   //If we comment this line then we will get error on line no. 14
+    val x: Int = 42.0
+
+    //2.
+    def y(implicit a:Int)=a
+    implicit val z:Int =10
+    println(y)
+
+    //3.
+    class Person1{
+      val worksAt:String = "xyz"
+    }
+
+    class Person2{
+      val designation:String = "developer"
+    }
+
+    val person1 = new Person1
+    implicit def xz(a:Person1):Person2 = new Person2
+    println(person1.designation)
+
+  }
+
+
+}
